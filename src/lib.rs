30eb0760040348eb0bd1a3801cfc969e04d3a41e
Christopher Nylen <joffynylen@hotmail.com>
2026-04-19 16:55:39 +0200

    Added derives

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,24 @@
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub enum TextureHandle {
     Test,
 }
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub enum ShaderHandle {
     DefaultSprite,
 }
+
+#[derive(Debug, Clone, Copy)]
 pub struct Vector2F {
     pub x: f32,
     pub y: f32,
 }
+#[derive(Debug, Clone, Copy)]
 pub struct Vector3F {
     pub x: f32,
     pub y: f32,
     pub z: f32,
 }
+#[derive(Debug, Clone, Copy)]
 pub struct Entity {
     pub position: Vector2F,
     pub size: Vector2F,
