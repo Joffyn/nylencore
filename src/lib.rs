c2d80c3d935c612cc8d6b51892e7477ecb9507e5
Christopher Nylen <joffynylen@hotmail.com>
2026-04-19 17:24:10 +0200

    More entity stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,20 @@
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, Clone, Copy, PartialEq, Default, Eq, PartialOrd, Ord)]
 pub enum TextureHandle {
-    Test,
+    #[default]
+    Default,
 }
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, PartialOrd, Ord)]
 pub enum ShaderHandle {
-    DefaultSprite,
+    #[default]
+    Sprite,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, Default)]
 pub struct Vector2F {
     pub x: f32,
     pub y: f32,
 }
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, Default)]
 pub struct Vector3F {
     pub x: f32,
     pub y: f32,
@@ -20,11 +22,40 @@ pub struct Vector3F {
 }
 #[derive(Debug, Clone, Copy)]
 pub struct Entity {
-    pub position: Vector2F,
-    pub size: Vector2F,
-    pub layer: u32,
-    pub texture: TextureHandle,
-    pub shader: ShaderHandle,
+    position: Vector2F,
+    size: Vector2F,
+    layer: u32,
+    texture: TextureHandle,
+    shader: ShaderHandle,
+}
+impl Default for Entity {
+    fn default() -> Self {
+        Self {
+            position: Default::default(),
+            size: Vector2F { x: 1.0, y: 1.0 },
+            layer: Default::default(),
+            texture: Default::default(),
+            shader: Default::default(),
+        }
+    }
+}
+
+impl Entity {
+    fn new(pos: Vector2F, sprite: TextureHandle) -> Self {
+        Entity {
+            position: pos,
+            size: Vector2F { x: 1.0, y: 1.0 },
+            layer: Default::default(),
+            texture: sprite,
+            shader: Default::default(),
+        }
+    }
+    fn get_pos(&self) -> &Vector2F {
+        &self.position
+    }
+    fn get_size(&self) -> &Vector2F {
+        &self.size
+    }
 }
 
 #[cfg(test)]
